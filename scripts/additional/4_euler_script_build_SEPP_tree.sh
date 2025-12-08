79cdd501b98b24119d980088e4c8cf222cc87722
llaurra-n <laura.nieba@gmail.com>
2025-12-08 17:43:48 +0100

    script sepp tree

---
+++ scripts/additional/4_euler_script_build_SEPP_tree.sh
@@ -0,0 +1,21 @@
+#!/bin/bash 
+#SBATCH --job-name=taxonomy       # Job name
+#SBATCH --output=tax_output.log   # Output file
+#SBATCH --error=tax_error.log     # Error log
+#SBATCH --time=24:00:00            # Adjust the time as needed
+#SBATCH --mem-per-cpu=14G          # Adjust memory per CPU as needed
+#SBATCH --cpus-per-task=8          # Number of CPUs per task
+#SBATCH --mail-type=END,FAIL #Email to me when finished
+
+cd $SCRATCH
+
+# paths 
+data_dir=/lnieba/microbiomes/phylogeny
+
+qiime fragment-insertion sepp \
+  --i-representative-sequences $data_dir/dada2_rep_seq.qza \
+  --i-reference-database $data_dir/silva-128-sepp-refs.qza \
+  --p-threads 4 \
+  --o-tree $data_dir/sepp-tree.qza \
+  --o-placements $data_dir/sepp-tree-placements.qza \
+  --verbose
