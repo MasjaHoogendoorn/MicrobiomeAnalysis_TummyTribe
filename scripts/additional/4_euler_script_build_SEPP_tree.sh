f0d56db0b28801551cb1bb119ab7d9aab0e3004e
llaurra-n <laura.nieba@gmail.com>
2025-12-11 09:45:15 +0100

    changes

--- scripts/additional/4_euler_script_build_SEPP_tree.sh
+++ scripts/additional/4_euler_script_build_SEPP_tree.sh
@@ -1,21 +1,20 @@
 #!/bin/bash 
-#SBATCH --job-name=taxonomy       # Job name
-#SBATCH --output=tax_output.log   # Output file
-#SBATCH --error=tax_error.log     # Error log
+#SBATCH --job-name=SEPP_tree       # Job name
+#SBATCH --output=sepp_output.log   # Output file
+#SBATCH --error=sepp_error.log     # Error log
 #SBATCH --time=24:00:00            # Adjust the time as needed
 #SBATCH --mem-per-cpu=14G          # Adjust memory per CPU as needed
 #SBATCH --cpus-per-task=8          # Number of CPUs per task
 #SBATCH --mail-type=END,FAIL #Email to me when finished
 
 cd $SCRATCH
+cd microbiomes
 
-# paths 
-data_dir=/lnieba/microbiomes
 
 qiime fragment-insertion sepp \
-  --i-representative-sequences $data_dir/dada2_rep_seq_filtered.qza \
-  --i-reference-database $data_dir/silva-128-sepp-refs.qza \
+  --i-representative-sequences dada2_rep_seq_filtered.qza \
+  --i-reference-database silva-128-sepp-refs.qza \
   --p-threads 4 \
-  --o-tree $data_dir/sepp-tree.qza \
-  --o-placements $data_dir/sepp-tree-placements.qza \
+  --o-tree sepp-tree.qza \
+  --o-placements sepp-tree-placements.qza \
   --verbose
